46c744c678bba370c30285f04d50a792387c55c8
Stepanov <lexa-chel@inbox.ru>
2017-05-28 11:21:46 +0500

    Debounce on input, some changes in css

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
 (function() {
 	
-	let string;
 	let inputEl = document.querySelector('input');
 	let canvasEl = document.querySelector('#qr');
+	let timer = null;
 	let qr = new QRious({
 			element: canvasEl,
 			size: 210
 		});
+		
+	inputEl.addEventListener('input', onOnput);	
 	
 	if (chrome.tabs) {
+		//generate qr code from tab url
 		chrome.tabs.getSelected(null, function(tab) {
-			string = tab.url;
-			//alert(string);
-			inputEl.value = string;
-			qr.value = string;
+			let url = tab.url;
+			inputEl.value = url;
+			generateQr(url);
 			inputEl.focus();
 			inputEl.select();
 		});
 	}
 	
+	function generateQr(string) {
+		let finalString = (string) ? string : 'Please enter a string'; 
+		qr.value = finalString;
+	}
+	
+	function onOnput() {
+		if (timer) {
+			clearTimeout(timer);
+		}  
+		timer = setTimeout(()=> {
+			generateQr(inputEl.value)
+			 
+		},300);
+	}
 	
 })();
